8f7db92905e707ad9b526fd586a389ce98485490
ST_Oster.Huang <oster.huang@criltds.com>
2021-06-22 18:09:17 +0800

    1.1 建立專案

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="test-pre-processor">
+      <div class="app-head">
+        這邊是標題
+      </div>
+      <div class="app-content">
+        這邊是內容
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style>
+html, body {
+  height: 100%;
+}
+
+html, body, div {
+  border: 0;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
+</style>
+
+<style lang="stylus" scoped>
+.test-pre-processor
+  display flex
+  height 100%
+  flex-direction column
+  .app-head
+    border 1px #ccc solid
+    padding 30px
+  .app-content
+    flex 1
+    margin-top 8px
+    border 1px #ccc solid
+    padding 30px
 </style>
